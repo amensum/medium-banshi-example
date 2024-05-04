30670d3d8004e8fae04f691daee0b6e069eabb76
amensum <amensum@ya.ru>
2024-05-04 18:38:38 +0300

    users and orders

---
+++ apps/medium-banshi-example-order/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'medium-banshi-example-order',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/medium-banshi-example-order',
+};
